0d8e47b67a581686e7171b011f69b527eda5447d
tobySolutions <adedejitobiloba7@gmail.com>
2023-04-05 15:58:50 +0100

    add pod group 14 submission

---
+++ Final-Project-Submission/pod-group-14/database.js
@@ -0,0 +1,11 @@
+const databaseJSON = {
+  python: "https://www.bmc.com/blogs/python-vs-java/",
+  vs: "https://www.bmc.com/blogs/python-vs-java/",
+  lion: "https://www.shmoop.com/quotes/lions-tigers-bears-oh-my.html",
+  and: "https://www.shmoop.com/quotes/lions-tigers-bears-oh-my.html",
+  tigers: "https://www.shmoop.com/quotes/lions-tigers-bears-oh-my.html",
+  bears: "https://www.shmoop.com/quotes/lions-tigers-bears-oh-my.html",
+  "c#": "https://www.guru99.com/java-vs-c-sharp-key-difference.html#:~:text=Java%20is%20a%20class%2Dbased,only%20in%20an%20unsafe%20mode.",
+};
+
+export default databaseJSON;
---
+++ Final-Project-Submission/pod-group-14/index.js
@@ -0,0 +1,56 @@
+import databaseJSON from "./database.js";
+
+const form = document.querySelector("form");
+const inputBox = document.querySelector("input");
+const list = document.querySelector("ul");
+const div = document.querySelector("#wrapper");
+const p = document.createElement("p");
+
+form.addEventListener("submit", handleSearch);
+
+function handleSearch(event) {
+  event.preventDefault();
+  list.innerHTML = "";
+  p.innerText = " ";
+
+  if (!inputBox.value) return;
+
+  let searchPhrase = inputBox.value.toLowerCase();
+  let words = searchPhrase.split(" ");
+  const uniqueWords = new Set(words);
+  words = [...uniqueWords];
+
+  const databaseKeys = Object.keys(databaseJSON);
+
+  let hasLinks = false;
+
+  words.map((word) => {
+    if (databaseKeys.includes(word)) {
+      const resultLink = databaseJSON[word];
+      console.log(databaseJSON[word]);
+
+      hasLinks = true;
+      const listItem = document.createElement("li");
+      const hyperLink = document.createElement("a");
+
+      hyperLink.setAttribute("href", resultLink);
+      hyperLink.setAttribute("target", "_blank");
+      hyperLink.innerText = resultLink;
+      listItem.append(hyperLink);
+      list.append(listItem);
+
+      return word;
+    }
+  });
+
+  if (hasLinks === false) {
+    p.innerText = "Not Result Found";
+    list.append(p);
+  }
+  inputBox.value = "";
+}
+
+// split the the words in the input to individual strings
+// then put the items in a set to handle duplicates
+
+// Loop through the database and match unique words to each word
